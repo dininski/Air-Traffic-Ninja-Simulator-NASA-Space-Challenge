476cba23226ce17500aaf3d0bec5eaf53ae0b059
Dimitar Nikolov <dimitur.nikolov.1986@gmail.com>
2013-04-21 13:18:34 +0300

    Flights processing
    FlightProcessor class should check in small interval all flights state.
    If there is some problems this class will raise events and try to solve
    the problems. If solution is not possible will raise events with higher
    warning level in order to request users attention.

---
+++ Ninja Air Control/Ninja Air Control/Processing/FlightEventArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjaAirControl.Processing
+{
+    public enum WarningLevel{Normal, High, Critical}
+
+    public class FlightEventArgs : EventArgs
+    {
+        public List<Flight> Flights { get; set; }
+
+        public WarningLevel WarningLevel { get; set; }
+
+        public FlightEventArgs()
+        {
+            Flights = new List<Flight>();
+        }
+
+        public FlightEventArgs(List<Flight> flights, WarningLevel warningLevel)
+        {
+            Flights = flights;
+            WarningLevel = warningLevel;
+        }
+    }
+}
